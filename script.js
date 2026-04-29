a50fa47891d6adb22816cfef7f4a69412db1b366
vitor-daniel015 <vi.daniel16@gmail.com>
2026-04-29 11:58:18 -0300

    fix: 4

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ export function mensagemBoasVindas(nome) {
 }
 
 export function maiorNumero(a, b) {
-  if (a > b) { 
-    return `${a} e maior que ${b}` ;
-  };
-  if (b > a) { 
-    return `${b} e maior que ${a}`;
-  };
+  if (a > b) return `${a} e maior que ${b}`;
+  if (b > a) return `${b} e maior que ${a}`;
+  return "Os números são iguais";
 }
