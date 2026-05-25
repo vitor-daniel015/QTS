289a06b667d41574d861340f53b49be8e45e13c8
vitor-daniel015 <vi.daniel16@gmail.com>
2026-05-25 08:10:17 -0300

    feat: ex lais

---
+++ _tests_/script.test.js
@@ -0,0 +1,34 @@
+const { maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre } = require('./script');
+
+test('maior numero', () => {
+  expect(maiorNumero(2, 3)).toBe(3);
+});
+
+test('mensagem de boas vindas', () => {
+  expect(mensagemBoasVindas('Lais')).toBe('Olá, Lais!');
+});
+test('somar dois numeros', () => {
+  expect(somar(2, 3)).toBe(5);
+});
+test('numero par ou impar', () => {
+  expect(ImparPar(2, 3)).toBe('O primeiro numero é par: 2', 'O segundo numero é impar: 3');
+});
+test('condição votar', () => {
+  expect(Votar(15)).toBe('Voce não pode votar');
+});
+test('condição numeros', () => {
+  expect(Numeros(-2)).toBe('Numero negativo');
+});
+test('condição bonus', () => {
+  expect(Bonus(5000)).toBe('Seu bônus é de: 500 e o total ficará 5500');
+});
+test('condição dirigir', () => {
+  expect(Dirigir(18)).toBe('Voce pode dirigir');
+});
+test('semestre', () => {
+  expect(Semestre('janeiro')).toBe('primeiro semstre');
+});
+
+
+
+
--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-function somar(a, b) {
-  return a + b;
-}
 
+function maiorNumero(a,b){
+  if (a>b){
+    return a
+  }
+  else {
+    return b 
+  }
+}
 function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
-
-function maiorNumero(a, b) {
-  if (a > b) return `${a} e maior que ${b}`;
-  if (b > a) return `${b} e maior que ${a}`;
+function somar(c,d){
+  return c + d
 }
+function ImparPar(a, b){
+  if (a%2==0) { return `O primeiro numero é par: ${a}` }
+  else if (a%2==1) {return `O primeiro numero é impar: ${a}`}
 
-module.exports = { somar, mensagemBoasVindas, maiorNumero };
+  if (b%2==0) { return `O segundo numero é par: ${b}` }
+  else { return `O segundo numero é impar: ${b}` }
+}
+function Votar(a){
+  if (a<16) { return `Voce não pode votar`}
+  if (a>=16 && a<18) { return `Voce tem voto opcional`}
+  if (a>=18) {return `Voce é obrigado a votar`}
+  if (a>=70) {return `Vocew tem voto opcional`}
+}
+function Numeros(a){
+  if(a>0) {return `Numero positivo`}
+  if(a==0) { return `Numero igual a zero`}
+  if(a<0) {return `Numero negativo`}
+}
+function Bonus(a){
+  if(a<2000){
+    conta = (20/100)*a
+    return `Seu bônus é de: ${conta} e o total ficará ${conta+a}`
+  }
+  if(a>=2000 && a<=5000){
+    conta = (10/100)*a
+    return `Seu bônus é de: ${conta} e o total ficará ${conta+a}`
+  }
+  if(a>5000){
+    conta = (5/100)*a
+    return `Seu bônus é de: ${conta} e o total ficará ${conta+a}`
+  }
+}
+function Dirigir(a){
+  if(a<18) {return `Voce não pode dirigir`}
+  if(a>=18) {return `Voce pode dirigir`}
+}
+function Semestre(a){
+  if(a== 'janeiro' || 'fevereiro' || 'março' || 'abril' || 'maio' || 'junho')
+  return `primeiro semestre`
+  if(a== 'julho' || 'agosto' || 'setembro' || 'outubro' || 'novembro' || 'dezembro')
+  return `segundo semestre`
+}
+// sla
+
+module.exports = {maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre};
