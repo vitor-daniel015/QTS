2d074f94ab6d741302652c4c80d4359d2b605354
vitor-daniel015 <vi.daniel16@gmail.com>
2026-05-25 08:33:42 -0300

    feat: finalizar git actions 1.0

--- _tests_/script.test.js
+++ _tests_/script.test.js
@@ -1,4 +1,4 @@
-const { maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre } = require('./script');
+const { maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre, Vogal, ReceberArray, Login } = require('../script');
 
 test('maior numero', () => {
   expect(maiorNumero(2, 3)).toBe(3);
@@ -7,26 +7,45 @@ test('maior numero', () => {
 test('mensagem de boas vindas', () => {
   expect(mensagemBoasVindas('Lais')).toBe('Olá, Lais!');
 });
+
 test('somar dois numeros', () => {
   expect(somar(2, 3)).toBe(5);
 });
+
 test('numero par ou impar', () => {
   expect(ImparPar(2, 3)).toBe('O primeiro numero é par: 2', 'O segundo numero é impar: 3');
 });
+
 test('condição votar', () => {
   expect(Votar(15)).toBe('Voce não pode votar');
 });
+
 test('condição numeros', () => {
   expect(Numeros(-2)).toBe('Numero negativo');
 });
+
 test('condição bonus', () => {
   expect(Bonus(5000)).toBe('Seu bônus é de: 500 e o total ficará 5500');
 });
+
 test('condição dirigir', () => {
   expect(Dirigir(18)).toBe('Voce pode dirigir');
 });
+
 test('semestre', () => {
-  expect(Semestre('janeiro')).toBe('primeiro semstre');
+  expect(Semestre('janeiro')).toBe('primeiro semestre');
+});
+
+test('vogal', () => {
+  expect(Vogal('a')).toBe('E vogal');
+});
+
+test('receber array', () => {
+  expect(ReceberArray([1,2,3,4,5])).toBe(3);
+});
+
+test('tela login', () => {
+  expect(Login('Admin','123')).toBe('Conectado com sucesso!');
 });
 
 
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function maiorNumero(a,b){
   if (a>b){
     return a
@@ -7,12 +6,15 @@ function maiorNumero(a,b){
     return b 
   }
 }
+
 function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
+
 function somar(c,d){
   return c + d
 }
+
 function ImparPar(a, b){
   if (a%2==0) { return `O primeiro numero é par: ${a}` }
   else if (a%2==1) {return `O primeiro numero é impar: ${a}`}
@@ -20,17 +22,20 @@ function ImparPar(a, b){
   if (b%2==0) { return `O segundo numero é par: ${b}` }
   else { return `O segundo numero é impar: ${b}` }
 }
+
 function Votar(a){
   if (a<16) { return `Voce não pode votar`}
   if (a>=16 && a<18) { return `Voce tem voto opcional`}
   if (a>=18) {return `Voce é obrigado a votar`}
   if (a>=70) {return `Vocew tem voto opcional`}
 }
+
 function Numeros(a){
   if(a>0) {return `Numero positivo`}
   if(a==0) { return `Numero igual a zero`}
   if(a<0) {return `Numero negativo`}
 }
+
 function Bonus(a){
   if(a<2000){
     conta = (20/100)*a
@@ -45,16 +50,41 @@ function Bonus(a){
     return `Seu bônus é de: ${conta} e o total ficará ${conta+a}`
   }
 }
+
 function Dirigir(a){
   if(a<18) {return `Voce não pode dirigir`}
   if(a>=18) {return `Voce pode dirigir`}
 }
+
 function Semestre(a){
   if(a== 'janeiro' || 'fevereiro' || 'março' || 'abril' || 'maio' || 'junho')
   return `primeiro semestre`
   if(a== 'julho' || 'agosto' || 'setembro' || 'outubro' || 'novembro' || 'dezembro')
   return `segundo semestre`
 }
-// sla
 
-module.exports = {maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre};
+function Vogal(letra) {
+  letra.toLowerCase()
+  if (letra == 'a' || 'e' || 'i' || 'o' || 'u') {
+    return 'E vogal'
+  }
+  else {
+    return 'Insira algo valido'
+  }
+}
+
+function ReceberArray(Array) {
+  const soma = Array.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+  const media = soma / Array.length;
+  return media
+}
+
+function Login(user, password) {
+  if (user == 'Admin' && password == '123') {
+    return 'Conectado com sucesso!'
+  }
+  else {
+    return 'Acesso negado! Usuário ou senha incorretos!'
+  }
+}
+module.exports = {maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre, Vogal, ReceberArray, Login};
